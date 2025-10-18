61155a0f68f631166595c463445c1d61c017f0da
domsprog <domsocarol@gmail.com>
2025-10-18 14:44:05 +0800

    Add summary comments to TreeNode class
    Added summary documentation for the TreeNode class.

--- BSTApp/BST_FUNTIONS/Methods/Node/Node.cs
+++ BSTApp/BST_FUNTIONS/Methods/Node/Node.cs
@@ -5,6 +5,20 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
+/* <Summary>
+
+ The TreeNode class represents a single node in a Binary Search Tree (BST).
+ Each node holds an integer value (Data) and has two possible child nodes: Left and Right.
+
+ - Data: Stores the value of the current node.
+ - Left: Points to the left child node (contains smaller values).
+ - Right: Points to the right child node (contains larger values).
+ - ToString(): Returns the node’s data as a string for easy display.
+ - Constructor: Initializes a new TreeNode with the given data and sets its child nodes to null.
+
+</Summary>
+*/ 
+
 namespace BST_Library.Methods.Node
 {
     public  class TreeNode
